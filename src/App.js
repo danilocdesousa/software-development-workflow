2b37661e02d24f69a4812f5705651ba2bf194508
danilocdesousa <45634291+danilocdesousa@users.noreply.github.com>
2022-08-28 15:25:18 -0300

    Add tasks to the workflow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import './App.css';
+import { useState } from "react";
 import Navbar from './components/Navbar/Navbar';
 import TaskList from './components/TaskList/TaskList';
 
+/* Generate a new task ID */
+
+let idAccumulator = 0;
+const generateId = () => {
+  return idAccumulator += 1;
+};
+
 function App() {
+
+  /* Add new tasks */
+
+  const [tasks, setTasks] = useState([]);
+  const addTask = (content, state) => {
+
+    /* Task structure */
+    
+    const newTask = {
+      id: generateId(),
+      content,
+      state
+    };
+
+    /* Add existing tasks to the task list */
+    
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
   return (
     <div className="App">
-      <header className="">
+      <header>
         <Navbar />
       </header>
       <main>
         <div className='workflow'>
-          <TaskList title={`No Status`} />
-          <TaskList title={`Backlog`} />
-          <TaskList title={`In Progress`} />
-          <TaskList title={`Code Review`} />
-          <TaskList title={`Test`} />
-          <TaskList title={`Done`} />
+          <TaskList title={`No Status`} onAddTask={addTask} tasks={tasks} />
+          <TaskList title={`Backlog`} onAddTask={addTask} tasks={tasks} />
+          <TaskList title={`In Progress`} onAddTask={addTask} tasks={tasks} />
+          <TaskList title={`Code Review`} onAddTask={addTask} tasks={tasks} />
+          <TaskList title={`Test`} onAddTask={addTask} tasks={tasks} />
+          <TaskList title={`Done`} onAddTask={addTask} tasks={tasks} />
         </div>
       </main>
     </div>
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import "./tasklist.css";
 
-export default function TaskList({ title }) {
-    const [count, setCount] = useState(0);
-    const increment = () => {
-        setCount((currentCount) => {
-            return currentCount + 1;
-        })
+export default function TaskList({ title, onAddTask, tasks }) {
+
+    /* Call the onAddTask with arguments */
+
+    const addTask = () => {
+        return onAddTask("New task", "pending");
     };
+
     return (
-        <div className="task-list task-list-column">
+        <div className="task-list">
             <div className="task-list-title">{title}</div>
             <div className="task-list-content">
-                <div>{count}</div>
-                <button onClick={increment}>Increment</button>
+                {tasks.map((task) => {
+                    return <div key={task.id}>{task.content}</div>;
+                })}
+            </div>
+            <div>
+                <button onClick={addTask}>Add item</button>
             </div>
         </div>
     );
 }
 
 TaskList.propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    onAddTask: PropTypes.func.isRequired, 
+    tasks: PropTypes.array.isRequired
 };
