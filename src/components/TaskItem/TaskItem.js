7480c91acca590a167a81eb5b5b05023bdcb2563
danilocdesousa <45634291+danilocdesousa@users.noreply.github.com>
2022-08-31 23:02:12 -0300

    Add event to edit tasks

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -15,10 +15,16 @@ export default function TaskItem({ id, content, taskState }) {
         setEditableTitle(newTitle);
     };
 
+    const keyDown = (e) => {
+        if (e.key === "Enter") {
+            setIsEditing(false);
+        }
+    };
+
     if (isEditing) {
-        return <input type={`text`} value={editableTitle} onChange={onTitleChange} />
+        return <input type={`text`} value={editableTitle} onChange={onTitleChange} onKeyDown={keyDown} />
     } else {
-        return <div onClick={(e) => { return setIsEditing(true) }}>{content}</div>
+        return <div onClick={(e) => { return setIsEditing(true) }}>{editableTitle}</div>
     }
     /* End show or edit the task */
 }
