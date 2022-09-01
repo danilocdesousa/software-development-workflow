f57e65b1911c1c59400616a49d489682d0bcac33
danilocdesousa <45634291+danilocdesousa@users.noreply.github.com>
2022-08-31 22:06:43 -0300

    Adds option to show or edit the task

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 export default function TaskItem({ id, content, taskState }) {
-    return <div>{content}</div>
+
+    /* Show or edit the task */
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editableTitle, setEditableTitle] = useState(content);
+    const onTitleChange = (e) => {
+
+        /* Gets input value */
+
+        const newTitle = e.target.value;
+        setEditableTitle(newTitle);
+    };
+
+    if (isEditing) {
+        return <input type={`text`} value={editableTitle} onChange={onTitleChange} />
+    } else {
+        return <div onClick={(e) => { return setIsEditing(true) }}>{content}</div>
+    }
+    /* End show or edit the task */
 }
 
 TaskItem.propTypes = {
