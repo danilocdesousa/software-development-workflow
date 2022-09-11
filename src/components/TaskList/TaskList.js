6b9aeeb243524a172e9b114c63d84edc2c71b1fb
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2022-09-11 15:09:22 -0300

    Update the style of the task list button

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -19,11 +19,15 @@ export default function TaskList({ title, onAddTask, tasks, onTaskUpdate, taskSt
             <div className="task-list-content">
                 {tasks.map((task) => {
                     return <TaskItem key={task.id} id={task.id}
-                        content={task.content} taskState={task.state} onTaskUpdate={onTaskUpdate}  onDeleteTask={onDeleteTask} />
+                        content={task.content} taskState={task.state} onTaskUpdate={onTaskUpdate} onDeleteTask={onDeleteTask} />
                 })}
             </div>
             <div>
-                <button onClick={addTask}>Add item</button>
+                <button className="plus-button" onClick={addTask}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-lg" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z" />
+                    </svg>
+                    New</button>
             </div>
         </div>
     );
