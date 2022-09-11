f50fc23f8a76d22341a688959fe8c94b2c17429c
Danilo de Sousa <45634291+danilocdesousa@users.noreply.github.com>
2022-09-11 14:39:27 -0300

    Fix the updateTasks

--- src/App.js
+++ src/App.js
@@ -41,11 +41,11 @@ function App() {
 
   /* Update the task according to the arguments */
 
-  const updateTasks = (id, title, state) => {
+  const updateTask = (id, content, state) => {
     setTasks((existingTasks) => {
       return existingTasks.map((task) => {
         if (task.id === id) {
-          return { ...task, title, state };
+          return { ...task, content, state };
         } else {
           return task;
         }
@@ -76,32 +76,32 @@ function App() {
             title={`No Status`}
             onAddTask={addTask}
             tasks={tasks.filter((task) => task.state === "No Status")}
-            onTaskUpdate={updateTasks} taskState="No Status" onDeleteTask={deleteTask} />
+            onTaskUpdate={updateTask} taskState="No Status" onDeleteTask={deleteTask} />
           <TaskList
             title={`Backlog`}
             onAddTask={addTask}
             tasks={tasks.filter((task) => task.state === "Backlog")}
-            onTaskUpdate={updateTasks} taskState="Backlog" onDeleteTask={deleteTask} />
+            onTaskUpdate={updateTask} taskState="Backlog" onDeleteTask={deleteTask} />
           <TaskList
             title={`In Progress`}
             onAddTask={addTask}
             tasks={tasks.filter((task) => task.state === "In Progress")}
-            onTaskUpdate={updateTasks} taskState="In Progress" onDeleteTask={deleteTask} />
+            onTaskUpdate={updateTask} taskState="In Progress" onDeleteTask={deleteTask} />
           <TaskList
             title={`Code Review`}
             onAddTask={addTask}
             tasks={tasks.filter((task) => task.state === "Code Review")}
-            onTaskUpdate={updateTasks} taskState="Code Review" onDeleteTask={deleteTask} />
+            onTaskUpdate={updateTask} taskState="Code Review" onDeleteTask={deleteTask} />
           <TaskList
             title={`Test`}
             onAddTask={addTask}
             tasks={tasks.filter((task) => task.state === "Test")}
-            onTaskUpdate={updateTasks} taskState="Test" onDeleteTask={deleteTask} />
+            onTaskUpdate={updateTask} taskState="Test" onDeleteTask={deleteTask} />
           <TaskList
             title={`Done`}
             onAddTask={addTask}
             tasks={tasks.filter((task) => task.state === "Done")}
-            onTaskUpdate={updateTasks} taskState="Done" onDeleteTask={deleteTask} />
+            onTaskUpdate={updateTask} taskState="Done" onDeleteTask={deleteTask} />
         </div>
       </main>
     </div>
